9cfec9b62c4096f598a78cec620796f953896fd8
Luis Tovar <130521074+LTovarAlc@users.noreply.github.com>
2023-08-15 09:58:54 -0600

    page404

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
-import Home from "./pages/Home"
-import Sobre from ".pages/Sobre"
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Page404 from './pages/Page404';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
+
   return (
-    <>
-      <Home/>
-      <Sobre/>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Sobre' element={<Sobre/>}/>
+        <Route path='*' element={<Page404/>}/>
+      </Routes>
+    </Router>
   );
 }
 
